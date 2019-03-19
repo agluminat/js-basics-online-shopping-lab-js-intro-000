33092d07d7fedc6282d3fd640cdb285a8ebefff6
Maxi Learner <sagmeister@gmail.com>
2019-03-19 20:55:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,8 +48,15 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (item === cart[i].itemName) {
+      cart.splice(i, 1)
+      return cart
+    }
+    return "That item is not in your cart."
+  }
 }
+
 
 function placeOrder(cardNumber) {
   // write your code here
