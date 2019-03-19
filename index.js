00eec734cf461d232c897827611a3e0114820634
Maxi Learner <sagmeister@gmail.com>
2019-03-19 21:15:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
   else {
+    var sum = total()
     cart = []
     return `Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`
   }
