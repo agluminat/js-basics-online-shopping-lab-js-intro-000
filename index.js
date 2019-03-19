d2d2f7a818a26d36cf4bfe6d5657ee0912413e7c
Maxi Learner <sagmeister@gmail.com>
2019-03-19 21:16:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ return "That item is not in your cart."
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
-    console.log('Sorry, we don\'t have a credit card on file for you.')
+    return 'Sorry, we don\'t have a credit card on file for you.'
   }
   else {
     var sum = total()
